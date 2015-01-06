39eba84d71a77cf881ee23af0833435fbdbf9afe
Ryniu3001 <marcin3001@10g.pl>
2015-01-07 00:01:46 +0100

    New Project

---
+++ wycieczka/src/main/java/com/sample/Ask.java
@@ -0,0 +1,21 @@
+package com.sample;
+
+import javax.swing.JOptionPane;
+
+public final class Ask{
+	
+	public static boolean yesNo(String question, String title){		
+		return JOptionPane.showConfirmDialog(null, question, title, JOptionPane.YES_NO_OPTION) == 0;
+	}
+	
+	public static int inputInt(String question){		
+		return Integer.parseInt(JOptionPane.showInputDialog(question));
+	}
+	
+	public static String chooseOne(String question, String...args){
+		Object ob = JOptionPane.showInputDialog(null, question, "Input", JOptionPane.INFORMATION_MESSAGE, null,
+		args, args[0]);
+		
+		return ob.toString();
+	}
+}
---
+++ wycieczka/src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,202 @@
+package com.sample;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.logger.KnowledgeRuntimeLogger;
+import org.drools.logger.KnowledgeRuntimeLoggerFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DroolsTest {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+            KnowledgeBase kbase = readKnowledgeBase();
+            StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+            KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, "test");
+            // go !
+//            Message message = new Message();
+//            message.setMessage("Hello World");
+//            message.setStatus(Message.HELLO);
+//            ksession.insert(message);
+            ksession.fireAllRules();
+            logger.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    private static KnowledgeBase readKnowledgeBase() throws Exception {
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        kbuilder.add(ResourceFactory.newClassPathResource("Sample.drl"), ResourceType.DRL);
+        KnowledgeBuilderErrors errors = kbuilder.getErrors();
+        if (errors.size() > 0) {
+            for (KnowledgeBuilderError error: errors) {
+                System.err.println(error);
+            }
+            throw new IllegalArgumentException("Could not parse knowledge.");
+        }
+        KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+        kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+        return kbase;
+    }
+
+    public static class Age  {
+    	private int value;
+    	
+    	public Age(int v){
+    		value = v;
+    	}
+    	
+		public int getValue() {
+			return value;
+		}
+
+		public void setValue(int value) {
+			this.value = value;
+		}
+    	
+  
+    }
+    
+    public static class Budget {
+    	private int value;
+    	
+    	public Budget(int c){
+    		value = c;
+    	}
+		public int getValue() {
+			return value;
+		}
+
+		public void setValue(int value) {
+			this.value = value;
+		} 	
+    }
+    
+    public static class Sex {
+    	private String value;
+    	
+    	public Sex(String b){
+    		value = b;
+    	}	
+    }
+    
+    public static class Entertainment {
+    	private boolean value;
+    	
+    	public Entertainment(boolean n){
+    		value = n;
+    	}
+		public boolean isValue() {
+			return value;
+		}
+
+		public void setValue(boolean value) {
+			this.value = value;
+		} 	
+    }
+    
+    public static class Acrophobia { //lek wysokosci
+    	private boolean value;
+    	
+    	public Acrophobia(boolean l){
+    		value = l;
+    	}
+		public boolean isValue() {
+			return value;
+		}
+
+		public void setValue(boolean value) {
+			this.value = value;
+		}
+    }
+    
+    public static class Sea { //morze czy gory 
+    	private boolean value;
+    	
+    	public Sea(boolean b){
+    		value = b;
+    	}
+		public boolean isValue() {
+			return value;
+		}
+
+		public void setValue(boolean value) {
+			this.value = value;
+		}
+    }
+    
+    public static class Warm { 
+    	private boolean value;
+    	
+    	public Warm(boolean v){
+    		value = v;
+    	}
+		public boolean isValue() {
+			return value;
+		}
+
+		public void setValue(boolean value) {
+			this.value = value;
+		}
+    }
+    
+    public static class Color {
+    	private String value;
+    	
+    	public Color(String v){
+    		value = v;
+    	}
+    }
+    
+    public static class Culture { 
+    	private String value;
+    	
+    	public Culture(String v){
+    		value = v;
+    	}
+    }
+    
+    public static class FearOfDeath {
+    	private boolean value;
+    	
+    	public FearOfDeath(boolean b){
+    		value = b;
+    	}
+		public boolean isValue() {
+			return value;
+		}
+
+		public void setValue(boolean value) {
+			this.value = value;
+		}
+    }
+    
+    public static class FarTravel{
+    	private boolean value;
+    	
+    	public FarTravel(boolean b){
+    		setValue(b);
+    	}
+
+		public boolean isValue() {
+			return value;
+		}
+
+		public void setValue(boolean value) {
+			this.value = value;
+		}
+    	
+    	
+    }
+}
